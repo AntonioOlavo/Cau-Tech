1d0e1a3d5b98f51a30e4034a8909f888395629f3
AntonioOlavo <antonioolavo.175@gmail.com>
2024-03-05 16:16:00 -0300

    novos

--- serve.js
+++ serve.js
@@ -1,15 +1,30 @@
+//const express = require('express');
+
+//const caminho = require('path');
+
+//const app=express();
+
+//app.use(express.static(dirname + '/dist/cautech'));
+
+//app.get('/*', function(req,res) {
+
+//res.sendFile(caminho.join(dirname+
+
+//'/dist/cautech/index.html'));});
+
+//app.listen(process.env.PORT || 8080);
+
 const express = require('express');
+const path = require('path');
+const app = express();
 
-const caminho = require('path');
+app.use(express.static(path.join(__dirname, 'dist', 'cautech')));
 
-const app=express();
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'cautech', 'index.html'));
+});
 
-app.use(express.static(dirname + '/dist/cautech'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(caminho.join(dirname+
-
-'/dist/cautech/index.html'));});
-
-app.listen(process.env.PORT || 8080);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
